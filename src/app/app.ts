d0f78c9ab1df96a78d3f28dc2898d9005a02885a
Nooralhodaashraf <nooralhodaashraf@gmail.com>
2026-05-29 17:18:52 +0300

    feat: add topbar component with social links and contact information

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './layout/navbar/navbar';
 import { Footer } from './layout/footer/footer';
+import { Topbar } from './layout/topbar/topbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer, Topbar],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
