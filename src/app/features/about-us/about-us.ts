c584a2e3f32450ccbe3a1f67b12388711986be7d
Nooralhodaashraf <nooralhodaashraf@gmail.com>
2026-07-06 20:43:44 +0300

    finishing about

--- src/app/features/about-us/about-us.ts
+++ src/app/features/about-us/about-us.ts
@@ -5,10 +5,11 @@ import { Teachers } from './component/teachers/teachers';
 import { TeachingPhli } from './component/teaching-phli/teaching-phli';
 import { Special } from './component/special/special';
 import { Feedback } from '../home/components/feedback/feedback';
+import { Intro } from './component/intro/intro';
 
 @Component({
   selector: 'app-about-us',
-  imports: [Header, Vision, Teachers, TeachingPhli, Special, Feedback],
+  imports: [Header, Vision, Teachers, TeachingPhli, Special, Feedback, Intro],
   templateUrl: './about-us.html',
   styleUrl: './about-us.css',
 })
