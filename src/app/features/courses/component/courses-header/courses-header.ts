fe739ce234d9d3098d557064165104b8bf39a707
Nooralhodaashraf <nooralhodaashraf@gmail.com>
2026-07-14 05:54:32 +0300

    feat(courses): add Start and CoursesHeader components with initial templates and styles
    - Created CoursesHeader component with basic structure.
    - Added Start component with course categories and responsive design.
    - Implemented HTML and CSS for Start component.
    - Added unit tests for Start component.

---
+++ src/app/features/courses/component/courses-header/courses-header.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courses-header',
+  imports: [],
+  templateUrl: './courses-header.html',
+  styleUrl: './courses-header.css',
+})
+export class CoursesHeader {}
--- src/app/features/courses/courses.ts
+++ src/app/features/courses/courses.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CoursesHeader } from './component/courses-header/courses-header';
+import { Start } from './component/start/start';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [CoursesHeader, Start],
   templateUrl: './courses.html',
   styleUrl: './courses.css',
 })
