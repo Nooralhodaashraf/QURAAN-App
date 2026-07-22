6c9d389fe9fff7d1ac56aef453c1d9faa669c3dd
Nooralhodaashraf <nooralhodaashraf@gmail.com>
2026-07-22 23:38:15 +0300

    feat: add Learn component with HTML, CSS, and tests for course details

--- src/app/features/singl-course/singl-course.ts
+++ src/app/features/singl-course/singl-course.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CourseHeroS } from './components/course-hero-s/course-hero-s';
+import { Learn } from './components/learn/learn';
 
 @Component({
   selector: 'app-singl-course',
-  imports: [CourseHeroS],
+  imports: [CourseHeroS, Learn],
   templateUrl: './singl-course.html',
   styleUrl: './singl-course.css',
 })
