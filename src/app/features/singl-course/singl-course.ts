2439c783b1ebd370df6e1e2c9ff37a26b5547518
Nooralhodaashraf <nooralhodaashraf@gmail.com>
2026-07-26 16:18:14 +0300

    feat(whoto): add Whoto component with initial test suite
    - Created Whoto component with basic structure and template.
    - Added unit tests to verify component creation.

--- src/app/features/singl-course/singl-course.ts
+++ src/app/features/singl-course/singl-course.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CourseHeroS } from './components/course-hero-s/course-hero-s';
 import { Learn } from './components/learn/learn';
+import { Whoto } from './components/whoto/whoto';
 
 @Component({
   selector: 'app-singl-course',
-  imports: [CourseHeroS, Learn],
+  imports: [CourseHeroS, Learn, Whoto],
   templateUrl: './singl-course.html',
   styleUrl: './singl-course.css',
 })
