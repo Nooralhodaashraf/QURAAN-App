8407886882963ee38d54cf1128e570edea8bd711
Nooralhodaashraf <nooralhodaashraf@gmail.com>
2026-08-13 04:25:36 +0300

    feat(course-work): add CourseWork component with initial test suite

--- src/app/features/singl-course/singl-course.ts
+++ src/app/features/singl-course/singl-course.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CourseHeroS } from './components/course-hero-s/course-hero-s';
 import { Learn } from './components/learn/learn';
 import { Whoto } from './components/whoto/whoto';
+import { CourseWork } from './components/course-work/course-work';
 
 @Component({
   selector: 'app-singl-course',
-  imports: [CourseHeroS, Learn, Whoto],
+  imports: [CourseHeroS, Learn, Whoto, CourseWork],
   templateUrl: './singl-course.html',
   styleUrl: './singl-course.css',
 })
